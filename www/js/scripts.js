024bb5efa3b3a95d2c346da616def67b2eeb7c5a
Derek K <etx313@gmail.com>
2016-04-12 10:52:03 -0400

    bump version number

--- www/js/scripts.js
+++ www/js/scripts.js
@@ -761,38 +761,6 @@ angular.module('cbt')
 
 
 angular.module('cbt')
-	.controller('HomeController', function ($scope, $state, appVersion) {
-
-    $scope.version = appVersion;
-
-    $scope.items = [
-      {
-        name: 'Watch CAN Packets',
-        icon: 'ion-settings',
-        sref: 'logger',
-      },
-      {
-        name: 'Pipe CAN packets to Wireshark',
-        icon: 'ion-arrow-right-c',
-        sref: 'pipe',
-      },
-      {
-        name: 'Settings',
-        icon: 'ion-gear-b',
-        sref: 'settings',
-      },
-    ];
-
-    $scope.listItemClick = function(n){
-      $state.go( $scope.items[n].sref );
-    }
-
-});
-
-'use strict';
-
-
-angular.module('cbt')
 	.controller('HWStatusController', function ($rootScope, $scope, $state, $http, $interval, $timeout, $ionicModal, HardwareService) {
 
     $scope.navTitle = "Hardware";
@@ -942,6 +910,38 @@ angular.module('cbt')
 
 
 	});
+
+'use strict';
+
+
+angular.module('cbt')
+	.controller('HomeController', function ($scope, $state, appVersion) {
+
+    $scope.version = appVersion;
+
+    $scope.items = [
+      {
+        name: 'Watch CAN Packets',
+        icon: 'ion-settings',
+        sref: 'logger',
+      },
+      {
+        name: 'Pipe CAN packets to Wireshark',
+        icon: 'ion-arrow-right-c',
+        sref: 'pipe',
+      },
+      {
+        name: 'Settings',
+        icon: 'ion-gear-b',
+        sref: 'settings',
+      },
+    ];
+
+    $scope.listItemClick = function(n){
+      $state.go( $scope.items[n].sref );
+    }
+
+});
 
 'use strict';
 
@@ -3837,8 +3837,6 @@ angular.module('cbt')
 					commands.bus3logOn = [0x03, 0x03, 0x01];
 				break;
 			}
-
-			console.log(commands);
 
 		}
 
